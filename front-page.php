e101e07c5e29f0d7f4d243a07f5a6566ed1cc20a
joshvillahermosa <joshvillahermosa@gmail.com>
2014-12-16 20:20:41 -0600

    [dev] Styled jumbotron

--- front-page.php
+++ front-page.php
@@ -3,11 +3,22 @@
  * Template Name: Front-Page
  * Description: Front page
  */
+$ptbackgroundImg = wp_get_attachment_image_src(103); //Need to implement
 get_header(); ?>
+<style type="text/css">
+	/**
+	 * CSS for the front-page
+	 */
+	.pt-banner{
+		/*Will need to add a method here to dynamically add*/
+		background: url('http://pilartanmd.joshvee.com/wp-content/uploads/2014/12/ptjumbotron-compressed.jpg') no-repeat center center fixed;
+		background-size: cover;
+	}
+</style>
 <div class="jumbotron pt-banner">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-7">
+			<div class="col-md-7 pt-spirtual">
 				<!-- Where widget one will be located-->
 				<?php
 				/**
@@ -24,7 +35,6 @@ get_header(); ?>
 				/**
 				 *  Front Page jumbotron widget - Primarily used to feature products
 				 */
-					echo is_active_sidebar('front-page-2')." - Sidebar 2"; 
 					if (is_active_sidebar('front-page-2')) {
 					    dynamic_sidebar('front-page-2');
 					}
