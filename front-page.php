7b35c5e17cafe4b324773a3132214b8ea428fe6a
joshvillahermosa <joshvillahermosa@gmail.com>
2014-12-16 18:24:57 -0600

    [dev] Added front-page template

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Template Name: Front-Page
+ * Description: Foront page
+ */
+get_header(); ?>
+<div class="jumbotron pt-banner">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-7">
+				<!-- Where widget one will be located-->
+				<h1>Qoute</h1>
+				<p> Occupy meggings beard, Shoreditch pop-up meditation tilde normcore selvage tote bag cardigan PBR&B letterpress fanny pack. Brooklyn cray Marfa, mlkshk pickled kogi tilde trust fund dreamcatcher authentic single-origin coffee fixie. Yr YOLO viral quinoa photo booth disrupt</p>
+			</div>
+			<div class="col-md-5">
+				<!-- Where store widget will be located-->
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer();?>
