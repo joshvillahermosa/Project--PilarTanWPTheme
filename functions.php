5b039559da0853dec417d7393ec162fb222300a0
joshvillahermosa <joshvillahermosa@gmail.com>
2014-12-29 14:11:33 -0600

    [dev] Added modal and widget for modal

--- functions.php
+++ functions.php
@@ -226,6 +226,15 @@ function pilartan_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	register_sidebar( array(
+		'name'          => __( 'Front Page - Request Modal', 'pilartan' ),
+		'id'            => 'front-page-10',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 	/**
 	 * Add custom social media links here
 	 */
