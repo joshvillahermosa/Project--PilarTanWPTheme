21bf47519863bed3b25dd294f96dfd48d04a605d
joshvillahermosa <joshvillahermosa@gmail.com>
2014-12-29 14:11:55 -0600

    [dev] Removed extra white space from css change

--- header.php
+++ header.php
@@ -28,13 +28,13 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'pilartan' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php //_e( 'Skip to content', 'pilartan' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+		<!--<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //Blog name not needed//bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php /*Uncommented for blogger/ info bloginfo( 'description' ); */?></h2>
-		</div><!-- .site-branding -->
+		</div> .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation navbar navbar-default" role="navigation">
 			<div class="container">
