ac4d773b0fa61ef1c15ca04a4c714c01dc58c2cc
joshvillahermosa <joshvillahermosa@gmail.com>
2014-12-29 14:11:27 -0600

    [dev] Added modal and widget for modal

--- front-page.php
+++ front-page.php
@@ -52,12 +52,12 @@ get_header(); ?>
 					</div>
 					<div class="col-md-6" id="pt-product-details">
 						<div class="row" >
+							<div class="col-md-12" id="pt-product-link"><?php if (is_active_sidebar('front-page-9')) {dynamic_sidebar('front-page-9');}?></div>
+						</div>
+						<div class="row">
 							<div class="col-md-12" id="pt-product-headlineqoute">
 								<?php if (is_active_sidebar('front-page-8')) {dynamic_sidebar('front-page-8');}?>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12" id="pt-product-link"><?php if (is_active_sidebar('front-page-9')) {dynamic_sidebar('front-page-9');}?></div>
 						</div>
 					</div>
 				</div>
@@ -206,5 +206,24 @@ get_header(); ?>
 			</div>
 		</div>
 	</div>
+</div>
+
+<!--Home page modals-->
+
+<div class="modal fade" id="requestform" tabindex="-1" role="dialog" aria-labelledby="requestform" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="requestform">Thank you for taking interest in my book!</h4>
+      </div>
+      <div class="modal-body">
+        <?php if (is_active_sidebar('front-page-10')) {dynamic_sidebar('front-page-10');}?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
 </div>
 <?php get_footer();?>
