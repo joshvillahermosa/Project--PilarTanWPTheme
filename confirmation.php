ae581dc0520c0b849ffa87bdf799fd87aa865e68
joshvillahermosa <joshvillahermosa@gmail.com>
2015-01-03 14:58:59 -0600

    [dev] Added thank you page

---
+++ confirmation.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: Confirmation Page
+ * Description: Front page
+ */
+?>
+<?php get_header(); ?>
+<div class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><?php the_title()?></h1>
+				<!-- Displays widget-->
+				<div class='col-md-6'>
+				<?php if (is_active_sidebar('confirm-page-1')) 
+					{dynamic_sidebar('confirm-page-1');}?>
+				</div>
+				<!-- Displays content -->
+				<div class='col-md-6'>
+					<?php the_content();?>
+				</div>
+			<?php endwhile; else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>	
+</div>
+<?php get_footer();?>
+
--- functions.php
+++ functions.php
@@ -235,6 +235,19 @@ function pilartan_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+
+	/**
+	* Sign up confirmation widget
+	*/
+	register_sidebar( array(
+		'name'          => __( 'Confirmation Page  Widget', 'pilartan' ),
+		'id'            => 'confirm-page-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 	/**
 	 * Add custom social media links here
 	 */
