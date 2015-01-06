d7d6afe014a5231e9711fa401ca39384b05ae373
joshvillahermosa <joshvillahermosa@gmail.com>
2015-01-05 18:47:16 -0600

    [dev] Added comments to know what pages do

--- content-single.php
+++ content-single.php
@@ -4,6 +4,7 @@
  */
 ?>
 
+<!-- Displays single content posts-->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
