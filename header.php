7f29df683982d1e25714a8307fc2210c77d0754e
joshvillahermosa <joshvillahermosa@gmail.com>
2015-01-07 21:04:18 -0600

    [dev] Added favicon

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
